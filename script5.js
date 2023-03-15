91f62458d347adcfdfc9d67c142088c3af67ff28
andraparaschiv23 <andramparaschiv97@gmail.com>
2023-03-15 15:16:38 +0200

    new changes

--- script5.js
+++ script5.js
@@ -66,7 +66,9 @@ const rest2 = {
 rest1.numGuests ??= 10;
 rest2.numGuests ??= 10;
 
-rest1.owner = rest1.owner && '<ANONTMOUS>'; //shortcut when value is falsy
-rest2.owner = rest2.owner && '<ANONTMOUS>'; //shortcut when value is falsy
+//rest1.owner = rest1.owner && '<ANONTMOUS>'; //shortcut when value is falsy
+//rest2.owner = rest2.owner && '<ANONTMOUS>'; //shortcut when value is falsy
+rest1.numGuests &&= '<ANONYMOUS>'; //assign a value to a variable if it is thruthy
+rest2.numGuests &&= '<ANONYMOUS>';
 console.log(rest1);
 console.log(rest2);
